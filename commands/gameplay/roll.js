ca894d07f0cc1756e71eea7c6c09920c7e9e1ae0
TomCodess <tom_codes@outlook.com>
2025-02-14 22:17:22 -0500

    Cutting out to put body logic in for roll

--- commands/gameplay/roll.js
+++ commands/gameplay/roll.js
@@ -27,28 +27,7 @@ module.exports = {
 				return interaction.reply('You need to set up a profile first!');
 			}
 
-			const lastRoll = user.lastRoll || 0;
-			const now = Date.now();
-
-			// 5 minutes in milliseconds
-			const cooldown = 5 * 60 * 1000;
-
-			if (now - lastRoll < cooldown) {
-				const remainingTime = cooldown - (now - lastRoll);
-				const minutes = Math.floor(remainingTime / 60000);
-				const seconds = Math.floor((remainingTime % 60000) / 1000);
-				return interaction.reply(`You can work again in ${minutes} minutes and ${seconds} seconds.`);
-			}
-
-			// Random amount between 50 and 100
-			const coins = Math.floor(Math.random() * 51) + 50;
-			const newBalance = (user.balance || 0) + coins;
-			const updateDoc = {
-				$set: {
-					balance: newBalance,
-					lastRoll: now,
-				},
-			};
+            
 
 			await profiles.updateOne({ discordId: userId }, updateDoc);
 
