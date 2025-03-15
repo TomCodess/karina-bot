2c558b2bf7c3e87bb334b8744174dd8951efe72e
TomCodess <tom_codes@outlook.com>
2025-03-15 02:07:05 -0400

    code doc

--- commands/gameplay/sellCardHandler.js
+++ commands/gameplay/sellCardHandler.js
@@ -9,7 +9,8 @@ const db = new Pool({
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_NAME,
-	port: process.env.DB_PORT || 5432, // Default to 5432 if not provided
+    // Default to 5432 if not provided
+	port: process.env.DB_PORT || 5432,
 	ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
 });
 
