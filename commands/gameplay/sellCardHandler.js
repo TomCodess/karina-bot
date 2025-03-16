2d9eb1c02f2d2bda267610cbab414bdc7a18154a
TomCodess <tom_codes@outlook.com>
2025-03-16 02:21:33 -0400

    commenting changes for later

--- commands/gameplay/sellCardHandler.js
+++ commands/gameplay/sellCardHandler.js
@@ -23,7 +23,7 @@ module.exports = async function handleSellCard(interaction, selectedCard) {
      * ADD HERE LATER
      *  If the card is a rare card, sell it for 100 coins
      * if the card is a common card, sell it for 50 coins
-     * ..
+     * ..etc
      */
 
 	// Generate random sell price (50-70 coins)
