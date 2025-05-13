b0c821f5a7d60eb5c8b52ad9e705aa4e4e3fc70e
TomCodess <tom_codes@outlook.com>
2025-05-13 15:13:04 -0400

    etc

--- commands/gameplay/roll.js
+++ commands/gameplay/roll.js
@@ -143,6 +143,7 @@ module.exports = {
 
 		// Store roll data in message metadata **UNUSED might need later?s**
 		// const rollData = { selectedCards, message, userId };
+		
 
 		// Automatically delete the stitched image after sending
 		setTimeout(() => {
